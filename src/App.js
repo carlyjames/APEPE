16c100a89739768c850ba9e7c48fe91c6a06b3f7
Elisha Okon <eliscodavid@gmail.com>
2023-05-16 15:01:33 +0000

    fixed all bugs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'React'
+import React from 'react'
 import { Banner, Footer, Links, Listings } from './Components';
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'tailwindcss/tailwind.css'
-import Terms from './pages/terms';
-import Vision from './pages/vision';
 import { render } from "react-dom";
 import {
   BrowserRouter,
@@ -17,8 +15,8 @@ render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}></Route>
-      <Route path="/terms" element={<Terms />}></Route>
-      <Route path="/vision" element={<Vision />}></Route>
+     
+      
     </Routes>
   </BrowserRouter>,
   document.getElementById("root")
