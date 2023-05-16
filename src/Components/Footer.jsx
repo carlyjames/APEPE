5e654137cb72700ba857e3cecf3a3354d403ae0d
Elisha Okon <eliscodavid@gmail.com>
2023-05-16 14:36:12 +0000

    rebased and fixed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,16 +9,16 @@ const Footer = () => {
       <Divider />
       <div className='flex lg:flex-row flex-col items-center justify-around py-8 gap-4'>
         <Link to='/terms'>
-          <p className='hover:text-[#478807] ease'>Terms of use</p>
+          <p className='hover:text-[#964B00] ease'>Terms of use</p>
         </Link>
         <Link to='mailto: Team@AngryPepe.wtf'>
-          <p className='hover:text-[#478807] ease'>Contact Us</p>
+          <p className='hover:text-[#964B00] ease'>Contact Us</p>
         </Link>
         <Link to='/vision'>
-          <p className='hover:text-[#478807] ease'>Vision</p>
+          <p className='hover:text-[#964B00] ease'>Vision</p>
         </Link>
         <div>
-          <p> <CopyrightIcon /> 2023 Angry Pepe. It's okay to be angry pepe. </p>
+          <p> <CopyrightIcon /> 2023 Ho Lee $heet. It's okay to be Ho Lee $heet. </p>
           <p>Team @AngryPepe.wtf</p>
         </div>
       </div>
